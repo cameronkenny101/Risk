85533dcca4729aed029baf35614be1caed19a7e1
Ash <=>
2021-02-07 22:37:01 +0000

    Updated Documentation

--- src/test/java/Game/PlayerTest.java
+++ src/test/java/Game/PlayerTest.java
@@ -14,4 +14,14 @@ public class PlayerTest extends TestCase {
 
         assertEquals("Mark", player.getName());
     }
+
+    @Test
+    public void testColour() {
+        Player player = new Player("Cameron", Constants.PLAYER_COLOUR.RED);
+        assertEquals(Constants.PLAYER_COLOUR.RED, player.getColour());
+        Player player1 = new Player("Cameron", Constants.PLAYER_COLOUR.BLUE);
+        assertEquals(Constants.PLAYER_COLOUR.BLUE, player1.getColour());
+        Player player2 = new Player("Cameron", Constants.PLAYER_COLOUR.GREY);
+        assertEquals(Constants.PLAYER_COLOUR.GREY, player2.getColour());
+    }
 }
