2cf3d2eb62a59837d9ee0fd32f96298eb443b0f5
cameronkenny101 <cam.e.kenny@gmail.com>
2021-03-02 21:07:50 +0000

    Server now interacts with client side

--- src/main/java/SocketProgramming/ServerSideConnection.java
+++ src/main/java/SocketProgramming/ServerSideConnection.java
@@ -2,6 +2,7 @@ package SocketProgramming;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class ServerSideConnection implements Runnable {
@@ -14,10 +15,23 @@ public class ServerSideConnection implements Runnable {
     public ServerSideConnection(Socket s, int id) {
         socket = s;
         playerID = id;
+        try {
+            dataIn = new DataInputStream(socket.getInputStream());
+            dataOut = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Error in SSC method");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
-
+        try {
+            dataOut.writeInt(playerID);
+            dataOut.flush();
+        } catch (IOException e) {
+            System.out.println("Error in run method in SSC");
+            e.printStackTrace();
+        }
     }
 }
