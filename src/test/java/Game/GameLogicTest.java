e3924117acf42f18f35aaf739af23515649dc2fa
MarkOHalloran1776 <mark.ohalloran@ucdconnect.ie>
2021-03-17 14:42:05 +0000

    added each test their own contructor of GameLogicTest

--- src/test/java/Game/GameLogicTest.java
+++ src/test/java/Game/GameLogicTest.java
@@ -11,24 +11,23 @@ import java.util.ArrayList;
 
 public class GameLogicTest extends TestCase {
 
-    GameLogic logic = new GameLogic();
 
     @Test
     public void testTakeCountryColor() {
+        GameLogic logic = new GameLogic();
         Constants.PLAYER_COLOUR[] country_owner = {Constants.PLAYER_COLOUR.RED, Constants.PLAYER_COLOUR.ORANGE};
-        int[] troopCount = {2, 4};
         logic.takeCountryLogic(0, Constants.PLAYER_COLOUR.GREEN, 1);
         logic.takeCountryLogic(1, Constants.PLAYER_COLOUR.GREY, 2);
         assertEquals(country_owner[0], Constants.PLAYER_COLOUR.GREEN);
         assertEquals(country_owner[1], Constants.PLAYER_COLOUR.GREY);
-
     }
 
     @Test
     public void testTakeCountryTroops() {
+        GameLogic logic = new GameLogic();
         Constants.PLAYER_COLOUR[] country_owner = {Constants.PLAYER_COLOUR.RED, Constants.PLAYER_COLOUR.ORANGE, Constants.PLAYER_COLOUR.PURPLE};
         int[] troopCount = {0, 0, 0};
-        logic.takeCountryLogic(0, Constants.PLAYER_COLOUR.GREY, 2);
+        logic.takeCountryLogic(0, Constants.PLAYER_COLOUR.GREY, 3);
         logic.takeCountryLogic(1, Constants.PLAYER_COLOUR.GREY, 3);
         logic.takeCountryLogic(2, Constants.PLAYER_COLOUR.GREY, 5);
         assertEquals(2, troopCount[0]);
@@ -38,6 +37,7 @@ public class GameLogicTest extends TestCase {
 
     @Test
     public void testSetDiceToZero() {
+        GameLogic logic = new GameLogic();
         Player player1 = new Player("Cam", Constants.PLAYER_COLOUR.GREY);
         Player player2 = new Player("Mike", Constants.PLAYER_COLOUR.BLUE);
         player1.setDiceNum(2);
@@ -49,6 +49,7 @@ public class GameLogicTest extends TestCase {
 
     @Test
     public void testSetRandomCountries() {
+        GameLogic logic = new GameLogic();
         ArrayList<Integer> list = new ArrayList<>();
         logic.setRandomCountries();
         assertNotNull(list);
@@ -59,6 +60,7 @@ public class GameLogicTest extends TestCase {
 
     @Test
     public void testCalculateReinforcements(){
+        GameLogic logic = new GameLogic();
         Player testPlayer = new Player("Max", Constants.PLAYER_COLOUR.RED);
         for(int i = 0 ; i < 7 ; i ++) {
             logic.country_owner[i] = Constants.PLAYER_COLOUR.RED;
@@ -89,6 +91,7 @@ public class GameLogicTest extends TestCase {
 
     @Test
     public void testCalculateContReinforcements(){
+        GameLogic logic = new GameLogic();
         Player player = new Player("MARK", Constants.PLAYER_COLOUR.RED);
         for(int i = 0; i < 42;i++){
             logic.country_owner[i] = Constants.PLAYER_COLOUR.RED;
