5774832c9848aff25f6936084daca5298391f110
MarkOHalloran1776 <mark.ohalloran@ucdconnect.ie>
2021-03-17 14:34:04 +0000

    removed weird strings littered in test and removed a redundant test

--- src/test/java/Game/GameTest.java
+++ src/test/java/Game/GameTest.java
@@ -12,10 +12,9 @@ public class GameTest extends TestCase {
     public void testisWinner() {
         Game game = new Game();
         Player player = new Player("Mark", Constants.PLAYER_COLOUR.RED);
-<<<<<<< HEAD
-=======
+
         Player player2 = new Player("Paul", Constants.PLAYER_COLOUR.BLUE);
->>>>>>> 5d5c8d16c507176450ed6ce3db28e12fae6a008d
+
         GameLogic gameLogic = new GameLogic();
         Arrays.fill(gameLogic.country_owner, Constants.PLAYER_COLOUR.RED);
         assertTrue(game.isWinner(player,gameLogic.country_owner));
