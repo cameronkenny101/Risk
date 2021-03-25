dfab2d06b7a91094a5d9e86558b0a07dc05dd741
cameronkenny101 <cam.e.kenny@gmail.com>
2021-03-25 19:26:58 +0000

    Server tested and ran on Amazon EC2

--- src/main/java/Online/Server.java
+++ src/main/java/Online/Server.java
@@ -1,12 +1,15 @@
 package Online;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
 
-    protected ServerSocket ss;
+    private ServerSocket ss;
     protected int numPlayers;
     public ServerSideConnection player1;
     public ServerSideConnection player2;
@@ -18,7 +21,7 @@ public class Server {
         numPlayers = 0;
 
         try {
-            ss = new ServerSocket(3000);
+            ss = new ServerSocket(30000);
         } catch (IOException ex) {
             System.out.println("Error in Server constructor");
         }
@@ -221,6 +224,119 @@ public class Server {
             player1Turn = !player1Turn;
             if(!cont)
                 break;
+        }
+    }
+
+    public static class ServerSideConnection implements Runnable {
+
+        private Socket socket;
+        private DataInputStream dataIn;
+        private DataOutputStream dataOut;
+        private int playerID;
+        private String playerName;
+        private String playerColor;
+        private ArrayList<Integer> countries;
+
+        public ServerSideConnection(Socket s, int id) {
+            socket = s;
+            playerID = id;
+            try {
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                System.out.println("Error in SSC method");
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                sendPlayerID();
+            } catch (IOException e) {
+                System.out.println("Error in run method in SSC");
+                e.printStackTrace();
+            }
+        }
+
+        private void sendPlayerID() throws IOException {
+            dataOut.writeInt(playerID);
+            dataOut.flush();
+        }
+
+        public void getPlayer() throws IOException {
+            playerName = dataIn.readUTF();
+            playerColor = dataIn.readUTF();
+        }
+
+        public void sendPlayerInfo(String playerName, String playerColor) throws IOException {
+            dataOut.writeUTF(playerName);
+            dataOut.writeUTF(playerColor);
+            dataOut.flush();
+        }
+
+        public void getArray() throws IOException {
+            countries = new ArrayList<>();
+            int size = dataIn.readInt();
+            for(int i = 0; i < size; i++)
+                countries.add(dataIn.readInt());
+        }
+
+        public void sendArray(ArrayList<Integer> countries) throws IOException {
+            dataOut.writeInt(countries.size());
+            for (Integer country : countries) {
+                dataOut.writeInt(country);
+            }
+            dataOut.flush();
+        }
+
+        public int[] getIntArray() throws IOException {
+            int size = dataIn.readInt();
+            int[] array = new int[size];
+            for(int i = 0; i < size; i++)
+                array[i] = dataIn.readInt();
+            return array;
+        }
+
+        public void sendIntArray(int[] array) throws IOException {
+            dataOut.writeInt(array.length);
+            for (int i : array) {
+                dataOut.writeInt(i);
+            }
+            dataOut.flush();
+        }
+
+        public boolean getBoolean() throws IOException {
+            return dataIn.readBoolean();
+        }
+
+        public void sendBoolean(boolean nextMove) throws IOException {
+            dataOut.writeBoolean(nextMove);
+            dataOut.flush();
+        }
+
+        public int getInt() throws IOException {
+            int integer = dataIn.readInt();
+            System.out.println(integer);
+            return integer;
+        }
+
+        public void sendInt(int number) throws IOException {
+            System.out.println("Sending: " + number);
+            dataOut.writeInt(number);
+            dataOut.flush();
+        }
+
+        public String getPlayerColor() {
+            return playerColor;
+        }
+
+        public String getPlayerName() {
+            return playerName;
+        }
+
+        public ArrayList<Integer> getCountries() {
+            return countries;
         }
     }
 
