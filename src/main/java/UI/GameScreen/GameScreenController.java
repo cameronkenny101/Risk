7a4e04c18cc1cfc41b5673249e087d9bcedfe021
Ashraf Ali <ashrafali379@gmail.com>
2021-03-27 11:28:52 +0000

    Updated center code

--- src/main/java/UI/GameScreen/GameScreenController.java
+++ src/main/java/UI/GameScreen/GameScreenController.java
@@ -195,6 +195,14 @@ public class GameScreenController {
         outputText();
     }
 
+    private double centerX(Bounds bounds) {
+        return bounds.getMinX() + bounds.getWidth() / 2;
+    }
+
+    private double centerY(Bounds bounds) {
+        return bounds.getMinY() + bounds.getHeight() / 2;
+    }
+
     /**
      * Sets the label of each continent in the map
      */
@@ -202,39 +210,39 @@ public class GameScreenController {
         //Asia
         Bounds bounds = countries[22].getCountryPath().getBoundsInLocal();
         Asia.setY(bounds.getMinY() - 14);
-        Asia.setX(bounds.getMaxX() / 2 + 10);
+        Asia.setX(centerX(bounds) + 10);
         Asia.setRotate(15);
 
         //Europe
         bounds = countries[12].getCountryPath().getBoundsInLocal();
         Europe.setY(bounds.getMinY() - 15);
-        Europe.setX(bounds.getMaxX() / 2);
+        Europe.setX(centerX(bounds));
 
         //North America
         bounds = countries[8].getCountryPath().getBoundsInLocal();
         NA.setY(bounds.getMinY() - 30);
-        NA.setX(bounds.getMaxX() / 2);
+        NA.setX(centerX(bounds));
         NA.setRotate(-15);
 
         //South America
         bounds = Argentina.getBoundsInLocal();
-        SA.setY(bounds.getMaxY() / 2 - 10);
+        SA.setY(centerY(bounds) - 10);
         SA.setX(bounds.getMinX() - 170);
 
         //Africa
         bounds = Madagascar.getBoundsInLocal();
         Africa.setY(bounds.getMaxY() + 30);
-        Africa.setX(bounds.getMaxX() / 2 - 40);
+        Africa.setX(centerX(bounds) - 40);
 
         //Australia
         bounds = New_Guinea.getBoundsInLocal();
         Australia.setY(bounds.getMaxY() + 30);
-        Australia.setX(bounds.getMaxX() / 2 - 25);
+        Australia.setX(centerX(bounds) - 25);
 
         //Set Bounds for on hover country
         bounds = South_Africa.getBoundsInLocal();
         countryNameOnHover.setY(bounds.getMaxY() + 50);
-        countryNameOnHover.setX(bounds.getMaxX() / 2 - 100);
+        countryNameOnHover.setX(centerX(bounds) - 100);
     }
 
     /**
@@ -368,8 +376,8 @@ public class GameScreenController {
             circle = new Circle(10);
             countries[country_id].setTroopCircle(circle);
 
-            circle.setCenterX(bounds.getMaxX() / 2);
-            circle.setCenterY(bounds.getMaxY() / 2);
+            circle.setCenterX(centerX(bounds));
+            circle.setCenterY(centerY(bounds));
             // Quick fix for center of argentina
             if (path.getContent().equals("M227 487c1,0 3,-1 5,-2 0,2 1,5 1,8 6,2 5,8 10,10 1,-2 2,-4 3,-5 1,0 2,-1 3,-1 3,4 4,3 9,1 0,1 0,2 0,3 1,0 3,0 5,0 4,7 9,7 16,10 -1,4 -2,7 -4,11 4,-1 8,-1 12,-2 2,-4 2,-4 5,-6 1,1 1,2 2,3 -4,3 -10,8 -11,13 2,0 4,0 7,0 0,2 0,2 0,6 5,1 6,4 10,8 0,2 0,2 -3,2 0,1 0,2 -1,4 -8,3 -12,-1 -19,-1 5,5 5,5 7,8 0,0 1,0 1,-1 0,3 0,6 0,8 -3,2 -13,3 -16,5 -1,6 -1,6 -2,9 -2,0 -4,0 -6,0 0,-1 -1,-1 -1,-2 0,1 0,3 0,4 0,1 1,2 2,3 -3,1 -3,1 -3,2 1,0 2,0 2,0 0,1 0,2 0,3 0,0 -1,0 -2,0 0,1 0,2 1,3 -4,4 -4,4 -4,9 1,0 2,1 3,1 -1,3 -1,5 -1,8 -1,0 -2,1 -3,1 1,3 -2,8 -2,11 1,1 3,3 4,4 0,1 -1,1 -2,2 1,1 1,2 1,3 1,0 2,0 3,0 0,1 0,2 0,3 6,3 12,6 18,10 -14,4 -13,3 -26,0 1,-1 1,-1 1,-2 -1,0 -2,0 -3,0 0,-2 0,-3 0,-4 -2,0 -4,0 -5,0 0,-1 0,-2 0,-3 0,0 -1,1 -2,1 -1,-1 -3,-1 -5,-2 0,-2 0,-3 0,-4 -9,-5 -9,-14 -10,-23 -3,0 -3,0 -4,-2 1,-1 1,-2 1,-3 -1,0 -1,0 -2,0 0,-1 0,-3 0,-4 2,0 2,0 2,-1 -1,0 -1,0 -2,0 0,-2 0,-4 0,-6 1,0 2,0 4,0 0,-4 0,-9 0,-13 -1,0 -2,-1 -3,-1 5,-28 6,-61 4,-89 M309 631c-2,0 -3,-1 -4,-2 -1,-3 -1,-6 -1,-9 7,-7 9,-5 18,-4 0,1 0,3 1,5 -6,2 -7,3 -10,9 -2,1 -3,1 -4,1z M295 630c-1,-2 -1,-2 -4,-3 0,0 0,-1 0,-2 1,0 2,-1 4,-1 -1,-1 -2,-3 -3,-4 3,-2 6,-3 9,-4 0,3 0,7 1,10 -3,3 -3,3 -7,4z")) {
                 circle.setCenterX(circle.getCenterX() - 25);
