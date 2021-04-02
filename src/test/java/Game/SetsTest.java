196ca4d06e89a704f2f5db1c12b43b4072797153
MarkOHalloran1776 <mark.ohalloran@ucdconnect.ie>
2021-04-03 00:43:31 +0100

    TestIsValidSet

--- src/test/java/Game/SetsTest.java
+++ src/test/java/Game/SetsTest.java
@@ -33,6 +33,28 @@ class SetsTest extends TestCase {
 
     @Test
     void TestisValidSet() {
+        int[] sets = {3, 3, 0, 0};
+        int[] sets2 = {1, 1, 1, 0};
+        int[] sets3 = {3, 0, 0, 0};
+        int[] sets4 = {0, 3, 1, 1};
+        int[] sets5 = {0, 0, 2, 1};
+        int[] sets6 = {3, 1, 1, 0};
+        int[] sets7 = {1, 1, 1, 0};
+        int[] sets8 = {0, 0, 1, 2};
+        int[] sets9 = {0, 3, 1, 1};
+        int[] sets10 = {0, 0, 2, 1};
+
+        assertFalse(isValidSet(sets));
+        assertTrue(isValidSet(sets2));
+        assertTrue(isValidSet(sets3));
+        assertFalse(isValidSet(sets4));
+        assertTrue(isValidSet(sets5));
+        assertFalse(isValidSet(sets6));
+        assertTrue(isValidSet(sets7));
+        assertTrue(isValidSet(sets8));
+        assertFalse(isValidSet(sets9));
+        assertTrue(isValidSet(sets10));
+
     }
 
     @Test
