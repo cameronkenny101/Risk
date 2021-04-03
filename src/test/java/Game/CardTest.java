e8f868715daa0458b5d4de2b8c44c24cff34f135
MarkOHalloran1776 <mark.ohalloran@ucdconnect.ie>
2021-04-03 18:32:53 +0100

    getCardIndex

--- src/test/java/Game/CardTest.java
+++ src/test/java/Game/CardTest.java
@@ -10,6 +10,36 @@ class CardTest extends TestCase {
     @Test
     void getCardIndex() {
         Card card = new Card(0,5);
+        assertEquals(5,card.getCardIndex());
+
+        Card card2 = new Card(1,10);
+        assertEquals(10,card.getCardIndex());
+
+        Card card3 = new Card(2,21);
+        assertEquals(21,card.getCardIndex());
+    }
+
+    @Test
+    void setCardIndex() {
+        Card card = new Card(0,5);
+        assertEquals(5,card.getCardIndex());
+        card.setCardIndex(3);
+        assertEquals(3,card.getCardIndex());
+
+        Card card2 = new Card(0,7);
+        assertEquals(7,card.getCardIndex());
+        card.setCardIndex(8);
+        assertEquals(8,card.getCardIndex());
+
+        Card card3 = new Card(0,8);
+        assertEquals(8,card.getCardIndex());
+        card.setCardIndex(31);
+        assertEquals(31,card.getCardIndex());
+    }
+
+    @Test
+    void getInsignia() {
+        Card card = new Card(0,5);
         assertEquals(0,card.getInsignia());
 
         Card card2 = new Card(1,5);
@@ -17,16 +47,6 @@ class CardTest extends TestCase {
 
         Card card3 = new Card(2,5);
         assertEquals(2,card.getInsignia());
-    }
-
-    @Test
-    void setCardIndex() {
-        Card card = new Card(0,5);
-        assertEquals(0,card.getInsignia());
-    }
-
-    @Test
-    void getInsignia() {
     }
 
     @Test
