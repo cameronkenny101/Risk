de9bc4a6321b788ceb27d4069db8a9077b8ff3ff
MarkOHalloran1776 <mark.ohalloran@ucdconnect.ie>
2021-04-03 18:41:37 +0100

    test fixes and isWildCardTest

--- src/test/java/Game/CardTest.java
+++ src/test/java/Game/CardTest.java
@@ -13,10 +13,10 @@ class CardTest extends TestCase {
         assertEquals(5,card.getCardIndex());
 
         Card card2 = new Card(1,10);
-        assertEquals(10,card.getCardIndex());
+        assertEquals(10,card2.getCardIndex());
 
         Card card3 = new Card(2,21);
-        assertEquals(21,card.getCardIndex());
+        assertEquals(21,card3.getCardIndex());
     }
 
     @Test
@@ -27,14 +27,14 @@ class CardTest extends TestCase {
         assertEquals(3,card.getCardIndex());
 
         Card card2 = new Card(0,7);
-        assertEquals(7,card.getCardIndex());
-        card.setCardIndex(8);
-        assertEquals(8,card.getCardIndex());
+        assertEquals(7,card2.getCardIndex());
+        card2.setCardIndex(8);
+        assertEquals(8,card2.getCardIndex());
 
         Card card3 = new Card(0,8);
-        assertEquals(8,card.getCardIndex());
-        card.setCardIndex(31);
-        assertEquals(31,card.getCardIndex());
+        assertEquals(8,card3.getCardIndex());
+        card3.setCardIndex(31);
+        assertEquals(31,card3.getCardIndex());
     }
 
     @Test
@@ -43,22 +43,40 @@ class CardTest extends TestCase {
         assertEquals(0,card.getInsignia());
 
         Card card2 = new Card(1,5);
-        assertEquals(1,card.getInsignia());
+        assertEquals(1,card2.getInsignia());
 
         Card card3 = new Card(2,5);
-        assertEquals(2,card.getInsignia());
+        assertEquals(2,card3.getInsignia());
     }
 
     @Test
     void setInsignia() {
+        Card card = new Card(0,5);
+        assertEquals(0,card.getInsignia());
+        card.setInsignia(6);
+        assertEquals(6,card.getInsignia());
+
+        Card card2 = new Card(1,5);
+        assertEquals(1,card2.getInsignia());
+        card2.setInsignia(26);
+        assertEquals(26,card2.getInsignia());
+
+        Card card3 = new Card(12,5);
+        assertEquals(12,card3.getInsignia());
+        card3.setInsignia(36);
+        assertEquals(36,card3.getInsignia());
     }
 
     @Test
-    void isWildCard() {
-    }
+    void isWildCardANDsetWildCard() {
+        Card card = new Card(0,5);
+        card.setWildCard(true);
+        assertTrue(card.isWildCard());
 
-    @Test
-    void setWildCard() {
+        Card card2 = new Card(0,5);
+        card2.setWildCard(true);
+        assertTrue(card2.isWildCard());
+
     }
 
     @Test
