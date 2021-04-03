16919fa6f72d869753c87391625fd2a7251a015c
MarkOHalloran1776 <mark.ohalloran@ucdconnect.ie>
2021-04-03 18:30:37 +0100

    getCardIndex

--- src/test/java/Game/CardTest.java
+++ src/test/java/Game/CardTest.java
@@ -1,17 +1,28 @@
 package Game;
 
+import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CardTest {
+class CardTest extends TestCase {
 
     @Test
     void getCardIndex() {
+        Card card = new Card(0,5);
+        assertEquals(0,card.getInsignia());
+
+        Card card2 = new Card(1,5);
+        assertEquals(1,card.getInsignia());
+
+        Card card3 = new Card(2,5);
+        assertEquals(2,card.getInsignia());
     }
 
     @Test
     void setCardIndex() {
+        Card card = new Card(0,5);
+        assertEquals(0,card.getInsignia());
     }
 
     @Test
