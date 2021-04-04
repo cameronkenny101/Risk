43a65319c910a1ed7db45c762d1103c3e900751b
cameronkenny101 <cam.e.kenny@gmail.com>
2021-04-04 19:19:23 +0100

    Fixed Testing suites

--- src/test/java/Game/GameTest.java
+++ src/test/java/Game/GameTest.java
@@ -15,11 +15,11 @@ public class GameTest extends TestCase {
         Player player2 = new Player("Paul", Constants.PLAYER_COLOUR.BLUE);
 
         GameLogic gameLogic = new GameLogic();
-        Arrays.fill(gameLogic.country_owner, Constants.PLAYER_COLOUR.RED);
-        assertTrue(game.isLoser(player,gameLogic.country_owner));
-        gameLogic.country_owner[3] = Constants.PLAYER_COLOUR.BLUE;
-        assertFalse(game.isLoser(player,gameLogic.country_owner));
         Arrays.fill(gameLogic.country_owner, Constants.PLAYER_COLOUR.BLUE);
+        assertTrue(game.isLoser(player,gameLogic.country_owner));
+        gameLogic.country_owner[3] = Constants.PLAYER_COLOUR.RED;
+        assertFalse(game.isLoser(player,gameLogic.country_owner));
+        Arrays.fill(gameLogic.country_owner, Constants.PLAYER_COLOUR.RED);
         assertTrue(game.isLoser(player2,gameLogic.country_owner));
     }
 
